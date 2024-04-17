3e964faff04b4e70d4b39e8ec4311792860d9925
aishayusuff <aishatyusuffe@gmail.com>
2024-04-17 18:45:51 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import ProfileCard from './ProfileCard'
+
+function App() {
+ 
+  return (
+    <>
+      <h1>Personal Digital Assistant</h1>
+      <ProfileCard/>
+      <ProfileCard/>
+      <ProfileCard />
+     </>
+  )
+}
+
+export default App
---
+++ src/ProfileCard.jsx
@@ -0,0 +1,12 @@
+
+
+function ProfileCard() {
+ 
+    return (
+        <>
+        Profile Card 
+        </>
+    )
+}
+
+export default ProfileCard
