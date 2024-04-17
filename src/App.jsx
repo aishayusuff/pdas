1c36b5a91301e83ee40cf8fc1180e559c14fa9b6
aishayusuff <aishatyusuffe@gmail.com>
2024-04-17 21:01:43 +0200

    adds column styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import 'bulma/css/bulma.css'
 import ProfileCard from './ProfileCard'
 import AlexaImg from './assets/alexa.png';
 import CortanaImg from './assets/cortana.png';
@@ -9,9 +10,22 @@ function App() {
     <>
       <h1>Personal Digital Assistant</h1>
 
-      <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImg}/>
-      <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImg}/>
-      <ProfileCard title="Siri" handle="@siri01" image={SiriImg}/>
+        <div className='columns'>
+          <div className="column is-4">
+              <ProfileCard  title="Alexa" handle="@alexa99" image={AlexaImg} desc="Amazon's virtual assistant. Voice-controlled. Controls smart home devices, plays music, sets reminders, and more. Integrates with third-party apps."/>
+          </div>
+          <div className="column is-4">
+              <ProfileCard  title="Cortana" handle="@cortana32" image={CortanaImg} desc="Microsoft's virtual assistant. Voice-activated. Performs tasks, provides information, and organizes schedules. Integrated 
+              Customization
+              with Windows devices and Microsoft services."/>
+          </div>
+          <div className="column is-4">
+              <ProfileCard  title="Siri" handle="@siri01" image={SiriImg} desc="Apple's virtual assistant. Activated by voice. Handles tasks, answers questions, and controls Apple devices. Integrated with iOS, macOS, and Apple services."/> 
+          </div>
+         
+          
+         
+        </div>
      </>
   )
 }
--- src/ProfileCard.jsx
+++ src/ProfileCard.jsx
@@ -1,13 +1,29 @@
 /* eslint-disable react/prop-types */
 
-function ProfileCard({title, handle, image}) {
+function ProfileCard({title, handle, image, desc}) {
  
     return (
-        <>
-            <img src={image} alt="logo of a personal digital assistant" />
-            <h3>{title}</h3>
-            <h3>{handle}</h3> 
-        </>
+        <div className="card">
+            <div className="card-image">
+                <figure className="image is-1by1">
+                    <img src={image} alt="logo of a personal digital assistant" />
+                </figure>
+            </div>
+            <div className="card-content">
+                <div className="media-content">
+                    <p className="title is-4">{title}</p>
+                    <p className="subtitle is-6">{handle}</p>
+                </div>
+            </div>
+            <div className="content">
+                {desc}
+            </div>
+        </div>
+        // <>
+        //     
+        //     <h3></h3>
+        //     <h3>{handle}</h3> 
+        // </>
     )
 }
 
