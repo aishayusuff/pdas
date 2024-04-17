2ddc069a6203e3e1e79b7c73e4ee7b88385be9c0
aishayusuff <aishatyusuffe@gmail.com>
2024-04-17 20:29:00 +0200

    begins adding styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import ProfileCard from './ProfileCard'
+import AlexaImg from './assets/alexa.png';
+import CortanaImg from './assets/cortana.png';
+import SiriImg from './assets/siri.png'
 
 function App() {
  
   return (
     <>
       <h1>Personal Digital Assistant</h1>
-      <ProfileCard title="Alexa" handle="@alexa99"/>
-      <ProfileCard title="Cortana" handle="@cortana32"/>
-      <ProfileCard title="Siri" handle="@siri01"/>
+
+      <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImg}/>
+      <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImg}/>
+      <ProfileCard title="Siri" handle="@siri01" image={SiriImg}/>
      </>
   )
 }
--- src/ProfileCard.jsx
+++ src/ProfileCard.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable react/prop-types */
 
-
-function ProfileCard({title, handle}) {
+function ProfileCard({title, handle, image}) {
  
     return (
         <>
+            <img src={image} alt="logo of a personal digital assistant" />
             <h3>{title}</h3>
             <h3>{handle}</h3> 
         </>
